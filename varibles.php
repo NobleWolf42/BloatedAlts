b8adf20a0713477da81f8bc54f1afd65adbb72fa
NobleWolf42 <greenboyb1@gmail.com>
2021-09-20 15:47:55 -0400

    gitignore change

--- varibles.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$dbuser = "bloatedalts";
-	$dbpassword = "2oX5ES2Hv1tI8HCIVsRf";
-	$dbname = "bloatedalts";
-	$dbserver = "localhost";
-	$logintime = (60 * 60 * 6);
-	$cookie_name = "bloatedalts";
-?>
